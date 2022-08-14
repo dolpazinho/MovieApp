02e8af90e7b5a7dfc84af6414a98026958eb0ac7
Ola <dolpaz11@gmail.com>
2022-08-14 12:50:44 +0200

    Creating state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import { createStore } from 'vuex'
 
+const SET_USER = "SET_USER" //set user constant
+
 export default createStore({
   state: {
+    user: {}
   },
   getters: {
   },
   mutations: {
+    [SET_USER](state, user){
+      state.user = user
+    }
   },
   actions: {
   },
